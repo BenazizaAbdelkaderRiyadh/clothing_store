51ad769e2148fe2234c4255d829b219376bdbc54
BenazizaAbdelkaderRiyadh <a.benazizariyadh@gmail.com>
2025-09-24 17:39:47 +0100

    ProductImage.tsx

--- components/ProductImage.tsx
+++ components/ProductImage.tsx
@@ -10,13 +10,13 @@ const ProductImage: React.FC<ProductImageProps> = ({ image, brandText, category 
   const isShoe = category === 'Shoes';
 
   return (
-    // Outer group for parallax effect on background elements
+
     <div className="group relative w-full flex items-center justify-center my-4 cursor-pointer">
       <span className="absolute text-[9rem] font-bold text-gray-200/50 tracking-[1rem] select-none uppercase transition-transform duration-500 ease-out group-hover:-translate-x-4">
         {brandText}
       </span>
 
-      {/* Inner group for effects on the shoe */}
+      {}
       <div className={`group relative w-full max-w-2xl transition-transform duration-500 ease-in-out ${isShoe ? 'transform -rotate-15 hover:rotate-0 hover:scale-105' : 'hover:scale-105'}`}>
         <img 
           src={image} 
